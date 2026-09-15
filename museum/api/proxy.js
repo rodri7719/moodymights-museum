d32ba446ef06a3ceb7884d96a67a31d093ed85f7
rodri7719 <131325002+rodri7719@users.noreply.github.com>
2026-09-15 13:36:18 -0300

    123

--- museum/api/proxy.js
+++ museum/api/proxy.js
@@ -23,6 +23,7 @@ export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Content-Type', contentType);
     res.setHeader('Cache-Control', 'public, max-age=86400');
+    res.setHeader('Vercel-CDN-Cache-Control', 'public, s-maxage=86400, stale-while-revalidate=3600');
     res.send(Buffer.from(buffer));
   } catch (e) {
     res.status(500).send('Proxy error');
